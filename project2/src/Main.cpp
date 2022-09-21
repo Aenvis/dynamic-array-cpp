3f3e67ab5b3c2581f82ae9c8df46066e9da6e64a
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-09-21 15:19:07 +0200

    Added realoccating when exceeding array size

--- project2/src/Main.cpp
+++ project2/src/Main.cpp
@@ -3,31 +3,48 @@
 namespace vector
 {
 #define MAX_INIT_SIZE 1000
-	template<typename T>
 	class dynamic_array
 	{
 	private:
 		uint64_t m_size = -1;
-		T m_arr[MAX_INIT_SIZE]{};
-		int m_sizeIncrement=0;
+		uint64_t m_maxSize = MAX_INIT_SIZE;
+		int* m_arr = new int[m_maxSize];
+		int m_sizeIncrement=1;
 	public:
 		dynamic_array()
 			: m_size(0)
 		{
 			m_arr[m_size] = 0;
 		}
-
-		void emplace_back(T data)
+		~dynamic_array()
 		{
-			if (m_size < MAX_INIT_SIZE - 1)
-				m_arr[m_size++] = data;
-			else
-			{
-				
-			}
+			delete[] m_arr;
 		}
 
-		T operator[](int index)
+		void emplace_back(int data)
+		{
+			if (m_size >= m_maxSize)
+			{
+				std::cout << "Allocating new array twice the size\n";
+				m_sizeIncrement++;
+				m_maxSize = m_sizeIncrement * MAX_INIT_SIZE;
+				int* temp_arr = new int[m_maxSize];
+				for (size_t i = 0; i < (m_sizeIncrement - 1) * MAX_INIT_SIZE; i++)
+				{
+					temp_arr[i] = m_arr[i];
+				}
+				m_arr = temp_arr;
+				delete[] temp_arr;
+			}
+				m_arr[m_size++] = data;
+		}
+
+		int GetSize() const
+		{
+			return m_size;
+		}
+
+		int operator[](int index)
 		{
 			return m_arr[index];
 		}
@@ -35,8 +52,13 @@ namespace vector
 }
 int main()
 {
-	vector::dynamic_array<int> years;
-	years.emplace_back(1915);
-	std::cout << years[0];
+	vector::dynamic_array years;
+	for (size_t i = 0; i < 1002; i++)
+	{
+		years.emplace_back(i);
+	}
+		
+	std::cout << years.GetSize();
+
 	std::cin.get();
 }
